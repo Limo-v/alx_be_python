66d0e7617a38581607b3684eb04c2751e11ec8d4
limo-v <limookibet02@gmail.com>
2024-10-05 17:11:49 +1000

    main

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -10,7 +10,10 @@ def main():
     denominator = sys.argv[2]
 
     result = safe_divide(numerator, denominator)
-    print(result)
+    if result == None:
+        print()
+    else:
+       print(result)
 
 if __name__ == "__main__":
     main()
